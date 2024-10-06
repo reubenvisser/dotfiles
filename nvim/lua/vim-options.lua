7f847cbf9d2f83d167567da4dddca8f4f0a5714f
reubenvisser <reubenvisser@outlook.com>
2024-10-07 09:05:22 +1000

    nvim config

---
+++ nvim/lua/vim-options.lua
@@ -0,0 +1,13 @@
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+
+vim.g.mapleader = " "
+
+vim.wo.relativenumber = true
+
+-- Navigate vim panes better
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
