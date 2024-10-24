db5dee8c5e46ee48cda07c9fd794bce3238adfe0
Reuben Visser <rvisser@digi.com>
2024-10-25 08:36:46 +1000

    lsp remaps

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -18,12 +18,23 @@ return {
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+			local on_attach = function()
+				vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+				vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+				vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+				vim.keymap.set("n", "gr", require('telescope.builtin').lsp_references, {})
+				vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			end
+
 			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup({
-				capabilities = capabilities
+				capabilities = capabilities,
+				on_attach = on_attach,
 			})
 			lspconfig.pylsp.setup({
 				capabilities = capabilities,
+				on_attach = on_attach,
+
 				settings = {
 					pylsp = {
 						plugins = {
