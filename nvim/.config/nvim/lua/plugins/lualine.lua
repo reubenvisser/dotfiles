03d58fe65f0d9118ccd53823d067abb9215b7ae9
Reuben Visser <rvisser@digi.com>
2024-10-25 16:39:41 +1000

    update lualine to show full path

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -4,6 +4,15 @@ return {
 		require('lualine').setup({
 			options = {
 				theme = 'dracula'
+			},
+			sections = {
+				lualine_c = {
+					{
+						'filename',
+						file_status = true, -- displays file status (readonly status, modified status)
+						path = 1 -- 0 = just filename, 1 = relative path, 2 = absolute path
+					}
+				}
 			}
 		})
 	end
