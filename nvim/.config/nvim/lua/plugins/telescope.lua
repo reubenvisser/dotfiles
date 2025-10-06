e35871b839a794910a8ffd520c74a7055a593996
Visser, Reuben <rvisser@digi.com>
2025-10-07 08:25:45 +1000

    update vim config

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -7,6 +7,7 @@ return 	{
 			local builtin = require("telescope.builtin")
 			vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 			vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+			vim.keymap.set('n', '<C-h>', builtin.help_tags, {})
 			vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
 		end
 	},
