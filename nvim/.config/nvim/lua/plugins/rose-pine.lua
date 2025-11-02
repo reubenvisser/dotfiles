151a1ef08d54c6cba6ea8ad22b7a33b0f3919eaf
Visser, Reuben <rvisser@digi.com>
2025-11-03 07:21:46 +1000

    update themes

--- nvim/.config/nvim/lua/plugins/rose-pine.lua
+++ nvim/.config/nvim/lua/plugins/rose-pine.lua
@@ -1,7 +1,21 @@
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
-	config = function()
-		vim.cmd.colorscheme "rose-pine"
-	end
+    -- {
+    --     "rose-pine/neovim",
+    --     name = "rose-pine",
+    --     config = function()
+    --         vim.cmd.colorscheme "rose-pine"
+    --     end
+    -- }
+    {
+        "ellisonleao/gruvbox.nvim",
+        priority = 1000, -- load before all other plugins
+        config = function()
+            require("gruvbox").setup({
+                contrast = "hard",          -- options: "hard", "soft", or empty string
+                transparent_mode = false,   -- set to true if you want a transparent background
+            })
+            vim.o.background = "dark"     -- use the dark variant
+            vim.cmd([[colorscheme gruvbox]])
+        end,
+    },
 }
