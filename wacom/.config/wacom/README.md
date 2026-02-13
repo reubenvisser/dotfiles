bb9815825802e59d9cf5cf0e27896f8b124f35d4
reubenvisser <reubenvisser@outlook.com>
2026-02-13 11:17:58 +1000

    add full tablet support

---
+++ wacom/.config/wacom/README.md
@@ -0,0 +1,14 @@
+A place to put your wacom config so that it can be unique to setup. 
+
+Create a wacon.conf in here and add the following:
+
+```conf
+# ~/.config/wacom/wacom.conf
+# Monitor mappings (use xrandr --listmonitors to find names)
+LEFT="HDMI-A-0"
+MIDDLE="DisplayPort-1"
+RIGHT="DisplayPort-0"
+
+# Default monitor on init
+DEFAULT="middle"
+```
---
+++ wacom/.local/bin/wacom-setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Wacom Intuos S persistent configuration
+# Place in ~/dotfiles/scripts/wacom-setup.sh
+
+CONFIG="${XDG_CONFIG_HOME:-$HOME/.config}/wacom/wacom.conf"
+
+if [ ! -f "$CONFIG" ]; then
+    echo "Error: Config not found at $CONFIG"
+    echo "Create it with your monitor and device names."
+    exit 1
+fi
+
+source "$CONFIG"
+
+STYLUS="Wacom Intuos S Pen stylus"
+PAD="Wacom Intuos S Pad pad"
+
+init() {
+    # Wait for device to be ready
+    sleep 1
+    xsetwacom set "$STYLUS" MapToOutput "${MONITORS[$DEFAULT]}"
+
+    xsetwacom set "$PAD" Button 1 "key super F1"
+    xsetwacom set "$PAD" Button 2 "key super F2"
+    xsetwacom set "$PAD" Button 3 "key super F3"
+    xsetwacom set "$PAD" Button 8 "key super F4"
+}
+
+swap_to() {
+    xsetwacom set "$STYLUS" MapToOutput "${MONITORS[$1]}"
+}
+
+
+case "$1" in
+    init) init ;;
+    swap_to) swap_to "$2";;
+esac
