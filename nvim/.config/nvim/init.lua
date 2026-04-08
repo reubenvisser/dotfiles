572258efda2ad7faa5e4e5af94791665f317aabd
Visser, Reuben <reubenvisser@outlook.com>
2026-04-09 09:36:34 +1000

    upd: change to native lsp keymappings

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -170,10 +170,10 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(ev)
       local opts = { buffer = ev.buf }
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-      vim.keymap.set("n", "gr", require('telescope.builtin').lsp_references, opts)
+      -- vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+      -- vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+      -- vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+      vim.keymap.set("n", "grr", require('telescope.builtin').lsp_references, opts)
       vim.keymap.set("n", "K", function() vim.lsp.buf.hover({ border = "rounded" }) end, opts)
   end,
 })
